7936c08ca0a21ce855ac95da4d5aa3179c838ae2
snorresovold <snorresovold@gmail.com>
2022-05-29 23:14:05 +0200

    updates

---
+++ src/content/index.js
@@ -0,0 +1,46 @@
+export default {
+  nav: {
+    logo: 'SS',
+    links: [
+      { text: 'Work', to: 'mywork' },
+      { text: 'Contact', to: 'mycontact' },
+    ],
+  },
+  header: {
+    img:
+      '/assets/snorre.png',
+    text: ['Hallo!', "Ege er Snorre Søvold.", 'Eg er '],
+    typical: [
+      'web developer. 🖥',
+      2000,
+      'Youtuber. 📹',
+      2000,
+      'Mobile Developer',
+      2000,
+    ],
+    btnText: 'Discover More',
+  },
+
+  stack: {
+    title: 'Stack',
+    tech: [
+      {
+        img: process.env.PUBLIC_URL + '/assets/mongo.png',
+        alt: 'mongodb',
+      },
+      {
+        img: process.env.PUBLIC_URL + '/logo512.png',
+        alt: 'react',
+      },
+      {
+        img: process.env.PUBLIC_URL + '/assets/express.png',
+        alt: 'express',
+      },
+      {
+        img: process.env.PUBLIC_URL + '/assets/node.png',
+        alt: 'node',
+      },
+    ],
+    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only`,
+  },
+};
