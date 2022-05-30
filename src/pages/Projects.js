cbfbe6a49f007ff5b99612e37b481e29cec2b8c0
snorresovold <snorresovold@gmail.com>
2022-05-30 13:34:07 +0200

    updates to the project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,9 +7,14 @@ function Projects() {
   return (
     <div>
         <Navigation />
-        <div className='grid grid-cols-4 gap-4'>
-        {content.termin1.prosjekter.map((title, content) => {
-            
+        <p className=' text-3xl font-bold text-center mb-4'>Termin 1</p>
+        <p className=' text-xl font-bold text-center mb-4'>Dette er prosjekter som eg har hatt i første termin på haugaland vgs</p>
+
+        <div className='grid grid-cols-3 m-4'>
+        {content.termin1.prosjekter.map((input) => {
+            return (
+                <ProjectCard title={input.title} content={input.content} />
+            )
         })}
         </div>
     </div>
