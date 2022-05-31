3281549281262903138d232799258cbc71bbedac
snorresovold <snorresovold@gmail.com>
2022-05-31 12:22:55 +0200

    final updates for now

---
+++ build/precache-manifest.7f23fc6a559581ae2b58cb7594c24a87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a5a0fbd2c25e87b4e2bfe431726eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffdc44ff2b48a6f8eb82",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "0b1fec76b8b70769063d",
+    "url": "/static/css/main.6dc6fc8b.chunk.css"
+  },
+  {
+    "revision": "ffdc44ff2b48a6f8eb82",
+    "url": "/static/js/2.ecceee3e.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.ecceee3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1fec76b8b70769063d",
+    "url": "/static/js/main.70f6aef6.chunk.js"
+  },
+  {
+    "revision": "51b1263d09889fed3508",
+    "url": "/static/js/runtime-main.07ad49ca.js"
+  }
+]);
