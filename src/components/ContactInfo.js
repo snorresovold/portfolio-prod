bf65de44e6d4653445d4d43bafaa1e3d416ca291
snorresovold <snorresovold@gmail.com>
2022-05-31 12:12:08 +0200

    updates

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -23,13 +23,13 @@ function ContactInfo() {
                 </p>
                 </header>
                 <div className="icons-container inline-flex flex-col my-20">
-                <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+                <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-indigo-500 p-4">
                     <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    className="bi bi-telephone-fill h-4 w-4 text-blue-500"
+                    className="bi bi-telephone-fill h-4 w-4 text-indigo-500"
                     viewBox="0 0 16 16"
                     >
                     <path
@@ -41,13 +41,13 @@ function ContactInfo() {
                     {content.contact.phone}
                     </p>
                 </div>
-                <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+                <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-indigo-500 p-4">
                     <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    className="bi bi-envelope-fill h-4 w-4 text-blue-500"
+                    className="bi bi-envelope-fill h-4 w-4 text-indigo-500"
                     viewBox="0 0 16 16"
                     >
                     <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
@@ -56,13 +56,13 @@ function ContactInfo() {
                     {content.contact.email}
                     </p>
                 </div>
-                <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+                <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-indigo-500 p-4">
                     <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    className="bi bi-pin-fill h-4 w-4 text-blue-500"
+                    className="bi bi-pin-fill h-4 w-4 text-indigo-500"
                     viewBox="0 0 16 16"
                     >
                     <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z" />
@@ -80,7 +80,7 @@ function ContactInfo() {
                 </label>
                 <input
                 type="text"
-                className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+                className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-indigo-500"
                 name="name"
                 />
                 <label htmlFor="email" className="text-sm text-gray-600 mx-4 mt-4">
@@ -88,7 +88,7 @@ function ContactInfo() {
                 </label>
                 <input
                 type="text"
-                className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+                className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-indigo-500"
                 name="email"
                 />
                 <label
@@ -100,12 +100,12 @@ function ContactInfo() {
                 <textarea
                 rows="4"
                 type="text"
-                className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+                className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-indigo-500"
                 name="message"
                 ></textarea>
                 <button
                 type="submit"
-                className="bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold"
+                className="bg-indigo-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold"
                 >
                 Send Mail
                 </button>
